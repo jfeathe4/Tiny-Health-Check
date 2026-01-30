7d146e68741c6cefc58e8a967923d679e07bec58
jfeathe <jamiefeatherstone1996@gmail.com>
2026-01-30 17:45:22 -0500

    adding main frontend code push

--- frontend/src/api/registeredUrls.ts
+++ frontend/src/api/registeredUrls.ts
@@ -1,12 +1,12 @@
 import { apiClient } from './client';
-import { RegisteredUrl } from '../models';
+import { RegisteredUrl, ApiResponse } from '../models';
 
 export const getRegisteredUrls = async () => {
-  const response = await apiClient.get<RegisteredUrl>('/url');
-  return response.data;
+  const response = await apiClient.get<ApiResponse<RegisteredUrl[]>>('/url');
+  return response.data.data;
 };
 
 export const createRegisteredUrl = async (url: string) => {
-  const response = await apiClient.post<RegisteredUrl>('/url', { url });
-  return response.data;
+  const response = await apiClient.post<ApiResponse<RegisteredUrl>>('/url', { url });
+  return response.data.data;
 };
