06cefd41cf1b554b45cffe3ef41425f04a25d049
jfeathe <jamiefeatherstone1996@gmail.com>
2026-02-01 19:19:49 -0500

    fixing dockerbuild and test suite

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -3,4 +3,12 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>/tests'],
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: { rootDir: '.' },
+      },
+    ],
+  },
 };
